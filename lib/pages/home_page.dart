b781541bf1535a613f3ad578d43eef2619b184bb
765sanjay <sanjaysrc765@gmail.com>
2025-05-28 10:49:02 +0530

    Revert "the flash is integrated with the customer_service_application"
    This reverts commit e50aa707ee0fb55661f75de3ea4fcfcea03ff1aa.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
 import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:sklyit/pages/explore.dart';
 import 'package:sklyit/pages/home.dart';
 import 'package:sklyit/pages/saved.dart';
@@ -10,11 +9,6 @@ import 'package:sklyit/screens/chat_screen/chat_dashboard.dart';
 import 'package:sklyit/pages/profile.dart';
 import 'package:sklyit/pages/recents.dart';
 import 'package:sklyit/utils/user_utils.dart';
-import 'package:sklyit/repository/screens/bottomnav/bottomnavscreen.dart';
-import 'package:sklyit/repository/providers/cart_provider.dart';
-import 'package:sklyit/repository/providers/address_provider.dart';
-import 'package:sklyit/repository/providers/profile_provider.dart';
-import 'package:sklyit/repository/providers/toggle_provider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -24,16 +18,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int currentPageIndex = 2; // Index of Home in pages list
-  int navBarIndex = 3; // Index of home icon in navigation bar (4th icon)
-
-  @override
-  void initState() {
-    super.initState();
-    // Ensure home icon is active by default
-    currentPageIndex = 2;
-    navBarIndex = 3;
-  }
+  int currentPageIndex = 2;
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +27,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
     void search() => setState(() {
           currentPageIndex = 0;
-          navBarIndex = 0;
           currentBody = pages.elementAt(currentPageIndex);
         });
 
@@ -66,104 +50,64 @@ class _MyHomePageState extends State<MyHomePage> {
       fit: BoxFit.fitWidth,
     );
 
-    return WillPopScope(
-      onWillPop: () async {
-        // When back button is pressed, ensure home icon is active
-        setState(() {
-          currentPageIndex = 2;
-          navBarIndex = 3;
-          currentBody = pages.elementAt(currentPageIndex);
-        });
-        return true;
-      },
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            HomePageProvider().reload();
-          },
-          child: const Icon(Icons.refresh),
-        ),
-        appBar: AppBar(
-          leading: icon,
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.notifications),
-              onPressed: () => print('Notifications'),
-            ),
-            IconButton(
-              onPressed: () => {
-                  Navigator.push(
-                    context, 
-                    MaterialPageRoute(
-                      builder: (context) => ChatDashboard(uid: UserUtils.user!["userId"],)
-                    )
-                  )
-              }, 
-              icon: const Icon(Icons.message_outlined),
-            )
-          ]
-        ),
-        bottomNavigationBar: CurvedNavigationBar(
-          onTap: (int index) {
-            if (index == 2) { // Flash icon index
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => MultiProvider(
-                    providers: [
-                      ChangeNotifierProvider(create: (_) => CartProvider()),
-                      ChangeNotifierProvider(create: (_) => AddressProvider()),
-                      ChangeNotifierProvider(create: (_) => ProfileProvider()),
-                      ChangeNotifierProvider(create: (_) => ToggleProvider()),
-                    ],
-                    child: BottomNavScreen(initialIndex: 0),
-                  ),
-                ),
-              ).then((_) {
-                // When returning from BottomNavScreen, ensure home icon is active
-                setState(() {
-                  currentPageIndex = 2; // Index of Home in pages list
-                  navBarIndex = 3; // Index of home icon in navigation bar (4th icon)
-                  currentBody = pages.elementAt(currentPageIndex);
-                });
-              });
-            } else {
-              setState(() {
-                // For indices after flash icon (3,4,5), subtract 1 to get correct page index
-                currentPageIndex = index > 2 ? index - 1 : index;
-                navBarIndex = index;
-                currentBody = pages.elementAt(currentPageIndex);
-              });
-            }
-          },
-          backgroundColor: Theme.of(context).colorScheme.secondary,
-          buttonBackgroundColor: Theme.of(context).colorScheme.primary,
-          index: navBarIndex,
-          items: const [
-            CurvedNavigationBarItem(
-                child: Icon(Icons.search_outlined),
-                label: ''),
-            CurvedNavigationBarItem(
-                child: Icon(Icons.history_outlined),
-                label: ''),
-            CurvedNavigationBarItem(
-                child: Icon(Icons.flash_on_rounded),
-                label: ''),
-            CurvedNavigationBarItem(
-              child: Icon(Icons.home_outlined),
-              label: '',
-            ),
-            CurvedNavigationBarItem(
-                child: Icon(Icons.star_border),
-                label: ''),
-            CurvedNavigationBarItem(
-              child: Icon(Icons.person), 
-              label: ''
-            ),
-          ],
-        ),
-        body: currentBody,
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          HomePageProvider().reload();
+        },
+        child: const Icon(Icons.refresh),
       ),
+      appBar: AppBar(
+        leading: icon,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () => print('Notifications'),
+          ),
+          IconButton(
+            onPressed: () => {
+                Navigator.push(
+                  context, 
+                  MaterialPageRoute(
+                    builder: (context) => ChatDashboard(uid: UserUtils.user!["userId"],)
+                  )
+                )
+            }, 
+            icon: const Icon(Icons.message_outlined),
+          )
+        ]
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        onTap: (int index) {
+          setState(() {
+            currentPageIndex = index;
+            currentBody = pages.elementAt(currentPageIndex);
+          });
+        },
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        buttonBackgroundColor: Theme.of(context).colorScheme.primary,
+        index: currentPageIndex,
+        items: const [
+          CurvedNavigationBarItem(
+              child: Icon(Icons.search_outlined),
+              label: ''),
+          CurvedNavigationBarItem(
+              child: Icon(Icons.history_outlined),
+              label: ''),
+              CurvedNavigationBarItem(
+            child: Icon(Icons.home_outlined),
+            label: '',
+          ),
+          CurvedNavigationBarItem(
+              child: Icon(Icons.star_border),
+              label: ''),
+          CurvedNavigationBarItem(
+            child: Icon(Icons.person), 
+            label: ''
+          ),
+        ],
+      ),
+      body: currentBody,
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,7 +13,6 @@ import flutter_secure_storage_macos
 import open_file_mac
 import path_provider_foundation
 import shared_preferences_foundation
-import speech_to_text_macos
 import sqflite_darwin
 import url_launcher_macos
 import video_player_avfoundation
@@ -27,7 +26,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
